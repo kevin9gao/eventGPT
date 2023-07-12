052a210bdc8d713e564ff57e08671fd3215c369f
kevin <kevingao52997@gmail.com>
2023-07-11 17:48:29 -0700

    took out test code for error handling and restore auth

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body });
-})
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
 
 module.exports = router;
