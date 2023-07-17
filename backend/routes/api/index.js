e0de65a6b0b25209cf860b495f3568239c240cb9
kevin <kevingao52997@gmail.com>
2023-07-17 14:15:14 -0700

    added session.js

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,17 +1,14 @@
 const router = require('express').Router();
+const sessionRouter = require('./session');
 
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
-
 const { restoreUser } = require('../../utils/auth.js');
-
 const { requireAuth } = require('../../utils/auth.js');
 
 
-router.post('/test', function(req, res) {
-  res.json({ requestBody: req.body });
-});
+router.use('/session', sessionRouter);
 
 
 module.exports = router;
