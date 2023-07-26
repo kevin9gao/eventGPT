a4783df9be752cc3fcde5163ef0d3762e2cb068b
kevin <kevingao52997@gmail.com>
2023-07-26 16:40:35 -0700

    a bit of work on the home page

--- frontend/src/components/Home/EventsList.js
+++ frontend/src/components/Home/EventsList.js
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { loadEvents } from "../../store/events";
+import './Home.css';
 
 export default function EventsList() {
   const dispatch = useDispatch();
+  const [eventType, setEventType] = useState('');
   const [location, setLocation] = useState('');
-  const [date, setDate] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
 
   const handleSubmit = e => {
     e.preventDefault();
 
     const payload = {
       location,
-      date,
+      startDate,
+      endDate,
     };
 
     dispatch(loadEvents(payload));
@@ -20,19 +24,43 @@ export default function EventsList() {
 
   return (
     <div className="events-list-wrapper">
-      <h1>Events</h1>
-      <form onSubmit={handleSubmit}>
-        <label>Location</label>
+      <h2 className="m-2">
+        What type of events are you looking for and where?
+      </h2>
+      <form
+        className="flex justify-center"
+        onSubmit={handleSubmit}>
+        <label>Show me</label>
         <input
           type="text"
+          placeholder="(type of event)"
+          value={eventType}
+          onChange={e => setEventType(e.target.value)}
+        />
+        <label>events in</label>
+        <input
+          type="text"
+          placeholder="(location)"
           value={location}
           onChange={e => setLocation(e.target.value)}
         />
-        <label>Date</label>
+        <label>during</label>
         <input
           type="text"
-          value={date}
-          onChange={e => setDate(e.target.value)}
+          placeholder="start date"
+          onFocus={e => e.target.type = 'date'}
+          onBlur={e => e.target.type = 'text'}
+          value={startDate}
+          onChange={e => setStartDate(e.target.value)}
+        />
+        <label>to</label>
+        <input
+          type="text"
+          placeholder="end date"
+          onFocus={e => e.target.type = 'date'}
+          onBlur={e => e.target.type = 'text'}
+          value={endDate}
+          onChange={e => setEndDate(e.target.value)}
         />
         <button>Search</button>
       </form>
